c30cd78e333ac13b66e5b36a0d4918128d9e178f
100528339BC <100528339@schools.edin.org>
2021-01-13 15:35:57 +0000

    a

--- Main.java
+++ Main.java
@@ -1,18 +1,32 @@
+import java.util.Scanner; 
+
 class Main {
   public static void main(String[] args) {
-    //Step 1: boolean valid set to false
-    //Step 2: start conditional loop while valild is false
-    //Step 3: ask user to enter password
-    //Step 4: set character firstLetter to the value of the first letter of password
-    //Step 5: set character lastLetter to the value of the last letter of password
-    //Step 6: integer firstValue to the ascii value of firstLetter
-    //Step 7: integer lastValue to the ascii value of lastLetter
-    //Step 8: If firstValue is between 40 and 42 AND lastValue is between 65 and 90 AND password length is greater than 7 THEN
-    //Step 9: valid = true
-    //Step 10: ELSE
-    //Step 11: display "Error password must be at least 8 characters, with a ()* to start adn a captial at the end"
-    //Step 12: END IF
-    //Step 13: END LOOP
-    //Step 14: display "Your password meets the strength requirements"
+
+    Scanner scan = new Scanner(System.in);
+
+    boolean valid = false;
+    String attempt;
+    char firstChar;
+    char lastChar;
+    int asciiFirst;
+    int asciiLast;
+
+    while (!valid){
+      System.out.println("enter password");
+      attempt = scan.next();
+      firstChar = attempt.charAt(0);
+      lastChar = attempt.charAt(attempt.length() - 1);
+
+      asciiFirst = (int)firstChar;
+      asciiLast = (int)lastChar;
+
+      if (asciiFirst >= 40 && asciiFirst <= 42 && asciiLast >= 65 && asciiLast <= 90 && attempt.length() >= 8){
+        valid = true;
+      }else { 
+        System.out.println("Error password must be at least 8 characters, with a ()* to start adn a captial at the end");
+      } 
+    }
+    System.out.println("Your password meets the strength requirements");
   }
 }
